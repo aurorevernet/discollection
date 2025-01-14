a82fa26a3a47ae5cbce02616c4f4ca5126233078
auroreo <aurore.vernet@live.com>
2025-01-14 16:09:28 +0100

    Initial Commit

---
+++ Discollection/DiscollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscollectionApp.swift
+//  Discollection
+//
+//  Created by Aurore Vernet on 14/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct DiscollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DiscollectionTests/DiscollectionTests.swift
@@ -0,0 +1,17 @@
+//
+//  DiscollectionTests.swift
+//  DiscollectionTests
+//
+//  Created by Aurore Vernet on 14/01/2025.
+//
+
+import Testing
+@testable import Discollection
+
+struct DiscollectionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
